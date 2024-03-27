2bf3c38d311c7fec6daf8a9869679281d023169e
Ivana Dimitrovska <ivana.z.dimitrovska@gmail.com>
2024-03-27 14:50:30 +0100

    second commit

--- src/main/java/com/example/deployexample2/DeployExample2Application.java
+++ src/main/java/com/example/deployexample2/DeployExample2Application.java
@@ -11,7 +11,7 @@ public class DeployExample2Application {
 
     @GetMapping("/message")
     public String message(){
-        return "Congrats ! you app deployed successfully in Azure !";
+        return "Congrats ! your application deployed successfully in Azure !";
     }
 
     public static void main(String[] args) {
