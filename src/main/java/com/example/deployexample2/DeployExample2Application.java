1d7b1bd18b6ec290ffd34a93069cccd3b7e3cd6f
Ivana Dimitrovska <ivana.z.dimitrovska@gmail.com>
2024-03-27 19:42:41 +0100

    third commit

--- src/main/java/com/example/deployexample2/DeployExample2Application.java
+++ src/main/java/com/example/deployexample2/DeployExample2Application.java
@@ -2,16 +2,28 @@ package com.example.deployexample2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 
-@SpringBootApplication
+import java.util.List;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @RestController
 public class DeployExample2Application {
 
-    @GetMapping("/message")
-    public String message(){
-        return "Congrats ! your application deployed successfully in Azure !";
+    private final Repo repo;
+
+    public DeployExample2Application(Repo repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping("/product")
+    public String message(Model model){
+        List<Product> lista=repo.findAll();
+        model.addAttribute("listaProdukti", lista);
+        return "ivana";
     }
 
     public static void main(String[] args) {
